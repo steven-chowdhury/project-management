7e71ff97eff63eab315d4fce9049143e6d982072
Steven Chowdhury <steven.chowdhury@gmail.com>
2025-01-16 00:52:50 -0800

    Add handler to return JSON error messages

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +16,26 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $exception, $request) {
+            $status = method_exists($exception, 'getStatusCode')
+                ? $exception->getStatusCode()
+                : 500;
+
+            if ($exception instanceof ValidationException) {
+                return response()->json([
+                    'message' => 'Validation error',
+                    'details' => $exception->errors(),
+                ], 400);
+            }
+
+            if($status == 404) {
+                return response()->json([
+                    'message'=> 'Resource not found',
+                ],404);
+            }
+
+            return response()->json([
+                'message' => $exception->getMessage() ?: 'Server error',
+            ], $status);
+        });
     })->create();
